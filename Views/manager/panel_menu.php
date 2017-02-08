33a5a69952d82102594f8a777ada990a4e0e0c03
innocentpice <innocent-pice@hotmail.com>
2017-02-08 06:20:02 +0000

    UPDATE MANAGER PANEL

--- Views/manager/panel_menu.php
+++ Views/manager/panel_menu.php
@@ -1,40 +1,33 @@
-<nav class="navbar navbar-inverse navbar-fixed-top">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="<?=URL_Path.'/manager'?>">Management System</a>
+<nav class="nav-side-menu col-sm-3">
+    <div class="brand">
+      <a href="<?=URL_Path.'/manager'?>">Management System</a>
     </div>
-    <div id="navbar" class="navbar-collapse collapse">
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="<?=URL_Path.'/manager/manual'?>">Manual</a></li>
-        <li><a href="<?=URL_Path.'/manager/help'?>">Help</a></li>
-        <li>
-          <a href="<?=URL_Path?>/manager/logout" style="display:inline;">
-            <button class="btn btn-danger btn-sm navbar-btn">
-            LOG OFF&nbsp;<span class="glyphicon glyphicon-log-out"></span>
-            </button>
-          </a>
-        </li>
-      </ul>
-    </div>
-  </div>
+    <i class="fa fa-bars fa-2x toggle-btn" data-toggle="collapse" data-target="#menu-content"></i>
+      <div class="menu-list">
+        <ul id="menu-content" class="menu-content collapse out">
+            <li>
+              <a href="#">
+              <i class="fa fa-home fa-lg"></i> HOME
+              </a>
+            </li>
+
+            <li  data-toggle="collapse" data-target="#nav_task" class="collapsed">
+              <a href="#"><i class="fa fa-money fa-lg"></i> JOB Manage <span class="arrow"></span></a>
+            </li>
+            <ul class="sub-menu collapse" id="nav_task">
+                <li><a href="<?=URL_Path.'/manager/tasklist'?>">JOB VIEW</a></li>
+                <li><a href="<?=URL_Path.'/manager/createtask'?>">CREATE JOB</a></li>
+            </ul>
+            
+            <li  data-toggle="collapse" data-target="#nav_supplier" class="collapsed">
+              <a href="#"><i class="fa fa-truck fa-lg"></i> SUPPLIER Manage <span class="arrow"></span></a>
+            </li>
+            <ul class="sub-menu collapse" id="nav_supplier">
+                <li><a href="<?=URL_Path.'/manager/supplier'?>">SUPPLIER VIEW</a></li>
+            </ul>
+        </ul>
+     </div>
 </nav>
-<div class="container-fluid clearfix">
-  <div claas="row">
-    <div class="col-sm-3 col-md-2 sidebar">
-      <ul class="nav nav-sidebar bg-info">
-        <li class="active"><a>TASK MENU</a></li>
-        <li><a href="<?=URL_Path.'/manager/tasklist'?>">|- TASKLIST</a></li>
-        <li><a href="<?=URL_Path.'/manager/createtask'?>">|- CREATE TASK</a></li>
-      </ul>
-      <ul class="nav nav-sidebar bg-info">
-        <li class="active"><a>Supplier MENU</a></li>
-        <li><a href="<?=URL_Path.'/manager/supplier'?>">|- Supplier</a></li>
-      </ul>
-    </div>
-    <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main" style="padding-top:5%;min-height:600px;">
+
+<div class="container-fluid clearfix col-sm-9 col-sm-offset-3" style="padding-top:5%;min-height:600px;">
+  
