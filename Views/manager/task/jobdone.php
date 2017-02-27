a690472e1830e85887e91b50f63a64f73179d30b
innocentpice <innocent-pice@hotmail.com>
2017-02-27 06:25:05 +0000

    UPDATE LAYOUT

--- Views/manager/task/jobdone.php
+++ Views/manager/task/jobdone.php
@@ -19,6 +19,9 @@
     echo '<th colspan="3">CUSTOM</th>';
     echo '</tr></thead>';
     echo '<tbody>';
+    if(empty($this->list_data[$j])){
+      echo '<tr><td class="text-center bg-success" colspan="9">ไม่มีข้อมูล</td></tr>';
+    }
     foreach($this->list_data[$j] as $data){
         echo '<tr>';
         for($i=0;$i<8;$i++){
