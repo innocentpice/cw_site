823c44b026948ba39bcfd9e4e52f7a78d3b9ecce
innocentpice <innocent-pice@hotmail.com>
2017-03-01 02:54:33 +0000

    FIX SEO

--- Views/sitemap/index.php
+++ Views/sitemap/index.php
@@ -1,9 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>http://www.cwgreat.co.th/</loc>
+    <loc><?=URL_Path?>/</loc>
   </url>
   <url>
-    <loc>http://www.cwgreat.co.th/checkjob</loc>
+    <loc><?=URL_Path?>/checkjob</loc>
   </url>
 </urlset>
