897c7071dbcf985dcdfad6ee77e34d25eed6f550
innocentpice <innocent-pice@hotmail.com>
2017-03-01 03:22:02 +0000

    FIX SEO

--- Views/header.php
+++ Views/header.php
@@ -9,6 +9,7 @@
         <meta name="keywords" content="บริษัทรับกลึงเหล็ก, โรงกลึง, งานกลึง, งานเชื่อม, อะไหล่เครื่องจักร, กลึงเหล็ก" />
         <meta name="robots" content="index,follow">
         <script type="application/ld+json"> { "@context": "http://schema.org", "@type": "Organization", "url": "http://www.cwgreat.co.th", "contactPoint": [{ "@type": "ContactPoint", "telephone": "+66 81-430-9217", "contactType": "customer service" }] } </script>
+        <script> (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-92819465-1', 'auto'); ga('send', 'pageview'); </script>
         <link rel="canonical" href="<?=URL_Path?>/">
         <link rel="stylesheet" href="https://necolas.github.io/normalize.css/5.0.0/normalize.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
